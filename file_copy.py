800ff90627e6d8511cdafb9df89dc3233d5cbcc7
Jared Bischof <jbischof@gmail.com>
2023-01-30 09:42:19 -0600

    Trying prom snapshot.

--- file_copy.py
+++ file_copy.py
@@ -1,9 +1,23 @@
+import requests
 from kubernetes import client, config
 
 def main():
+    prom_url = 'http://tartarus-prometheus.monitoring.svc.cluster.local:9090/api/v1/admin/tsdb/snapshot'
+    response = None
+    try:
+        response = requests.post(prom_url)
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)
+
+    if reponse.ok:
+        print response.status_code
+    else:
+        print "Response was not okay." + status_code
+
     config.load_incluster_config()
 
     v1 = client.CoreV1Api()
+
     print("Listing pods with their IPs:")
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
