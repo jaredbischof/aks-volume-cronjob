8a9724d0f3d1e3fa6b7530db26059c223d78fa08
Jared Bischof <jbischof@gmail.com>
2023-01-30 17:15:33 -0600

    Added more steps.

--- file_copy.py
+++ file_copy.py
@@ -1,6 +1,12 @@
+import file_manager
+import hashlib
+import logging
 import requests
+import tarfile
+import traceback
 from kubernetes import client, config
 from kubernetes.stream import stream
+from tempfile import TemporaryFile
 
 def main():
     prom_url = 'http://tartarus-prometheus.monitoring.svc.cluster.local:9090/api/v1/admin/tsdb/snapshot'
@@ -16,6 +22,7 @@ def main():
         print("Response was not OK")
 
     print("Status code = " + str(response.status_code))
+    snapshot_name = response.json()['data']['name']
 
     config.load_incluster_config()
 
@@ -24,15 +31,69 @@ def main():
     exec_command = [
         '/bin/sh',
         '-c',
-        '/bin/ls /prometheus/snapshots']
+        "/bin/tar zcvf /prometheus/snapshots/" + snapshot_name + ".tgz /prometheus/snapshots/" + snapshot_name]
 
     resp = stream(v1.connect_get_namespaced_pod_exec,
                   'prometheus-tartarus-prometheus-0',
                   'monitoring',
+                  container='prometheus',
                   command=exec_command,
                   stderr=True, stdin=False,
                   stdout=True, tty=False)
     print("Response: " + resp)
+
+    fp = "/prometheus/snapshots/" + snapshot_name + ".tgz"
+
+    exec_command = [
+        '/bin/sh',
+        '-c',
+        "/bin/md5sum " + fp]
+
+    resp = stream(v1.connect_get_namespaced_pod_exec,
+                  'prometheus-tartarus-prometheus-0',
+                  'monitoring',
+                  container='prometheus',
+                  command=exec_command,
+                  stderr=True, stdin=False,
+                  stdout=True, tty=False)
+    md5sum = resp.split(maxsplit = 1)[0]
+
+    dest_path = "./" + snapshot_name + ".tgz"
+    command_copy = ['tar', 'cf', '-', fp]
+    with TemporaryFile() as tar_buffer:
+        exec_stream = stream(v1.connect_get_namespaced_pod_exec,
+                             'prometheus-tartarus-prometheus-0',
+                             'monitoring',
+                             container='prometheus',
+                             command=command_copy, stderr=True, stdin=True, stdout=True, tty=False,
+                             _preload_content=False)
+        # Copy file to stream
+        try:
+            reader = file_manager.WSFileManager(exec_stream)
+            while True:
+                out, err, closed = reader.read_bytes()
+                if out:
+                    tar_buffer.write(out)
+                elif err:
+                    logging.debug("Error copying file {0}".format(err.decode("utf-8", "replace")))
+                if closed:
+                    break
+            exec_stream.close()
+            tar_buffer.flush()
+            tar_buffer.seek(0)
+            with tarfile.open(fileobj=tar_buffer, mode='r:') as tar:
+                member = tar.getmember(fp[1:])
+                tar.makefile(member, dest_path)
+                return True
+        except Exception as e:
+            logging.error(traceback.format_exc())
+
+    copied_md5sum = hashlib.md5(open(dest_path,'rb').read()).hexdigest()
+
+    if(md5sum == copied_md5sum):
+        print("Success!")
+    else:
+        print("Failed.")
 
 if __name__ == '__main__':
     main()
